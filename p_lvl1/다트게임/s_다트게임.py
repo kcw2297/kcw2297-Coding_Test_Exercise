0f07f3005fceb6bde9ece885b3097ada7da1a039
kcw2297 <chaeugg@gmail.com>
2022-01-14 22:11:35 +0900

    list comprehension과 if문을 이용하여 문자열에서 특정 기점으로 나눈다

--- p_lvl1/다트게임/s_다트게임.py
+++ p_lvl1/다트게임/s_다트게임.py
@@ -11,6 +11,13 @@ list 형식으로 단위를 나누었다
 거의 대부분 코딩테스트에서 list comprehension과 idx를 짝꿍으로 묶어가는게 맞아 보인다
 """
 
+"""
+문자열을 특정 기준으로 나눌 시에는 list comprehension(if문 이용)을 이용한다
+이후 list로 계산하기에 idx를 이용하여 원하는 계산을 한다
+i = -1은 처음 시작 기준을 0이 아닌 -1로 하여서 1씩 늘려가면서 원하는 순서를 정한다
+"""
+
+
 def solution(dartResult):
     answer = []
     dartResult = dartResult.replace('10','k')
