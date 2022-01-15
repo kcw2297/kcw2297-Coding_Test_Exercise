c04066e1cd7c3f51a214fb485250691be3bb46a3
kcw2297 <chaeugg@gmail.com>
2022-01-15 21:59:43 +0900

    이스케이프코드로 한줄띄기

---
+++ p_lvl1/직사각형별찍기/s_직사각형별찍기.py
@@ -0,0 +1,14 @@
+
+
+
+
+
+"""
+for loop을 사용하지 않고 이스케이프 코드로 작성한 것이다
+"""
+
+
+
+a, b = map(int, input().strip().split(' '))
+answer = ('*'*a +'\n')*b
+print(answer)
---
+++ p_lvl1/직사각형별찍기/t_직사각형별찍기.py
@@ -0,0 +1,9 @@
+"""https://programmers.co.kr/learn/courses/30/lessons/12969?language=python3"""
+
+
+
+
+a, b = map(int, input().strip().split(' '))
+for h in range(b):
+    l = "*"*a
+    print(l)
