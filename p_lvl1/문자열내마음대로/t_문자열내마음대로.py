e6bbc63ce2d36c38b3c267bc46d1bf087653a84c
kcw2297 <chaeugg@gmail.com>
2022-01-15 20:52:30 +0900

    리스트 안에 특정 기준으로 정렬시 sorted()안에 key를 활용한다

---
+++ p_lvl1/문자열내마음대로/t_문자열내마음대로.py
@@ -0,0 +1,13 @@
+def solution(strings, n):
+
+    ["abce", "abcd", "cdx"]	  2	 ["abcd", "abce", "cdx"]
+
+    n번째 위치에 있는 글자의 순으로 정렬
+    만약 n번째가 같다면 사전순으로 정렬
+
+    데이터타입은 리스트, IDX로 글자순 정렬 시도
+
+
+
+    for ele in reversed(range(len(strings))):
+        if strings[ele][n]>strings[ele][n]
