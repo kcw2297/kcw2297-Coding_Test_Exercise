6a28f4b4ce1ed747193f2970d41d2a650241c9a6
kcw2297 <chaeugg@gmail.com>
2022-01-24 18:30:22 +0900

    이번 문제는 정답과 동일하여 패스

---
+++ p_lvl1/핸드폰번호가리기/s_핸드폰번호가리기.py
@@ -0,0 +1,7 @@
+
+
+
+
+
+def solution(s):
+    return "*"*(len(s)-4) + s[-4:]
---
+++ p_lvl1/핸드폰번호가리기/t_핸드폰번호가리기.py
@@ -0,0 +1,7 @@
+"""https://programmers.co.kr/learn/courses/30/lessons/12948"""
+
+
+
+
+def solution(x):
+    return (len(x)-4)*'*'+x[-4:]
