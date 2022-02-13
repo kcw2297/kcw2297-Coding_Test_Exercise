a18e20d1d2137d13f560e34e4ce731efdf3355f7
kcw2297 <chaeugg@gmail.com>
2022-02-13 10:53:38 +0900

    bfs는 특정 노드들의 거리를 재는데 사용된다, 시작노드부터 다음 노드까지 거리를 측정하는 무언가를 정의해야한다

--- p_lvl2/거리두기확인/t_거리두기확인.py
+++ p_lvl2/거리두기확인/t_거리두기확인.py
@@ -4,4 +4,19 @@
 """
 bfs를 이용해 풀어낸 문제로 deque()를 정의하여 방문 노드를 체크하면서 다음
 노드와 비교한다
+
+배운점 :
+deque()안에는 iterable한 객체만 인자로 받는다 :
+deque()에허 iterable하지 않는 요소를 넣을경우 에러가 난다
+ex) [1,2]는 에러가 나오니 [[1,2]]로 list형식으로 만들어 넣어야한다
+
+pop()이후 받는 변수의 개수에 따라 데이터 형식이 다르게 나온다
+ex) a=[[1,2]]인 경우, x,y = a.pop()할시에는 1,2로 나누어 나온다
+
+visited와 distance로 2개의 table을 만들어 각 노드별 경로를 확인한다
+"""
+
+
+"""
+https://www.youtube.com/watch?v=hCVgKE6qwFs
 """
